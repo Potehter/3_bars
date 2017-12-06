75ec9a15881ebad1da4beb0ec1444d2e80ea5ae9
Dmitrii <poteher@gmail.com>
2017-12-07 00:08:18 +0300

    fix names of vars

--- bars.py
+++ bars.py
@@ -6,18 +6,18 @@ def load_data(filepath):
     	source_json = json.loads(source_file.read())
     return source_json
 
-def get_biggest_bar(data):
-    biggest_bar = max(data, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+def get_biggest_bar(bars_array):
+    biggest_bar = max(bars_array, key=lambda x: x['properties']['Attributes']['SeatsCount'])
     print('The biggest bar is:')
     print(json.dumps(biggest_bar, indent=4, ensure_ascii=False))
 
-def get_smallest_bar(data):
-    smallest_bar = min(data, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+def get_smallest_bar(bars_array):
+    smallest_bar = min(bars_array, key=lambda x: x['properties']['Attributes']['SeatsCount'])
     print('The smallest bar is:')
     print(json.dumps(smallest_bar, indent=4, ensure_ascii=False))
 
-def get_closest_bar(data, longitude, latitude):
-    closest_bar = min(data, key=lambda x: (((x['geometry']['coordinates'][0]-longitude)**2)+((x['geometry']['coordinates'][1]-latitude)**2))**0.5)
+def get_closest_bar(bars_array, longitude, latitude):
+    closest_bar = min(bars_array, key=lambda x: (((x['geometry']['coordinates'][0]-longitude)**2)+((x['geometry']['coordinates'][1]-latitude)**2))**0.5)
     print('The closest bar is:')
     print(json.dumps(closest_bar, indent=4, ensure_ascii=False))
 
